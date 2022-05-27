535d7ce637812f9e6fc5aa9df0a45fb55b978da1
Johan Euphrosine <proppy@google.com>
2022-05-28 01:44:34 +0900

    switch to micromamba

--- modules/silicon_design/scripts/build/images/provision.sh
+++ modules/silicon_design/scripts/build/images/provision.sh
@@ -27,8 +27,15 @@ mkdir -p ${PROVISION_DIR}
 gsutil -m rsync ${DAISY_SOURCES_PATH}/provision/ ${PROVISION_DIR}/ || true
 
 echo "DaisyStatus: installing conda-eda environment"
+<<<<<<< HEAD
 curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -C /usr/local -xvj bin/micromamba
 micromamba create --yes -r /opt/conda -n silicon --file ${PROVISION_DIR}/environment.yml
+=======
+cd /opt/conda && curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+/opt/conda/bin/micromamba install -vvv --yes -p /opt/conda --strict-channel-priority -c litex-hub -c main openroad=2.0_3818_g2ae9162aa open_pdks.sky130a magic netgen yosys=0.17 iverilog xls
+/opt/conda/bin/micromamba install -vvv --yes -p /opt/conda --strict-channel-priority -c conda-forge gdstk ngspice-lib tcllib
+/opt/conda/bin/python -m pip install pyyaml click pandas pyspice gdsfactory klayout scrapbook[gcs] google-cloud-aiplatform cloudml-hypertune
+>>>>>>> fe96f30 (switch to micromamba)
 
 echo "DaisyStatus: installing OpenLane"
 git clone --depth 1 -b ${OPENLANE_VERSION} https://github.com/The-OpenROAD-Project/OpenLane /OpenLane
@@ -37,10 +44,16 @@ echo "DaisyStatus: patching OpenLane"
 cp ${PROVISION_DIR}/install.tcl /OpenLane/configuration/
 echo ' install.tcl' >> /OpenLane/configuration/load_order.txt
 mkdir -p /OpenLane/install/build/versions
+<<<<<<< HEAD
 for tool in yosys netgen
 do
   /opt/conda/bin/conda list -c ${tool} > /OpenLane/install/build/versions/${tool}
 done
+=======
+cp ${PROVISION_DIR}/env.tcl /OpenLane/install/
+# https://github.com/The-OpenROAD-Project/OpenLane/pull/1027
+curl --silent  https://patch-diff.githubusercontent.com/raw/The-OpenROAD-Project/OpenLane/pull/1027.patch | patch -d /OpenLane -p1
+>>>>>>> fe96f30 (switch to micromamba)
 
 echo "DaisyStatus: adding profile hook"
 cp ${PROVISION_DIR}/profile.sh /etc/profile.d/silicon-design-profile.sh
