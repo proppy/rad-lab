fe96f307deb6b2e42cb24d3948f26245163ebb94
Johan Euphrosine <proppy@google.com>
2022-05-28 01:44:34 +0900

    switch to micromamba

--- modules/silicon_design/scripts/build/images/provision.sh
+++ modules/silicon_design/scripts/build/images/provision.sh
@@ -27,8 +27,10 @@ mkdir -p ${PROVISION_DIR}
 gsutil -m rsync ${DAISY_SOURCES_PATH}/provision/ ${PROVISION_DIR}/ || true
 
 echo "DaisyStatus: installing conda-eda environment"
-/opt/conda/bin/conda install --yes --prefix /opt/conda/ mamba
-/opt/conda/bin/mamba env update --prefix /opt/conda/ --file ${PROVISION_DIR}/environment.yml
+cd /opt/conda && curl -Ls https://micro.mamba.pm/api/micromamba/linux-64/latest | tar -xvj bin/micromamba
+/opt/conda/bin/micromamba install -vvv --yes -p /opt/conda --strict-channel-priority -c litex-hub -c main openroad=2.0_3818_g2ae9162aa open_pdks.sky130a magic netgen yosys=0.17 iverilog xls
+/opt/conda/bin/micromamba install -vvv --yes -p /opt/conda --strict-channel-priority -c conda-forge gdstk ngspice-lib tcllib
+/opt/conda/bin/python -m pip install pyyaml click pandas pyspice gdsfactory klayout scrapbook[gcs] google-cloud-aiplatform cloudml-hypertune
 
 echo "DaisyStatus: installing OpenLane"
 git clone --depth 1 -b ${OPENLANE_VERSION} https://github.com/The-OpenROAD-Project/OpenLane /OpenLane
@@ -36,10 +38,6 @@ git clone --depth 1 -b ${OPENLANE_VERSION} https://github.com/The-OpenROAD-Proje
 echo "DaisyStatus: patching OpenLane"
 mkdir -p /OpenLane/install/build/versions
 cp ${PROVISION_DIR}/env.tcl /OpenLane/install/
-for tool in yosys netgen
-do
-  /opt/conda/bin/conda list -c ${tool} > /OpenLane/install/build/versions/${tool}
-done
 # https://github.com/The-OpenROAD-Project/OpenLane/pull/1027
 curl --silent  https://patch-diff.githubusercontent.com/raw/The-OpenROAD-Project/OpenLane/pull/1027.patch | patch -d /OpenLane -p1
 
