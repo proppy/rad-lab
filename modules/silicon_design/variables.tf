ff62680f74ae71616382bcc70fd3320c9dbcf5d9
Johan Euphrosine <proppy@google.com>
2022-07-22 17:07:34 +0900

    modules/silicon_design: use name prefix

--- modules/silicon_design/variables.tf
+++ modules/silicon_design/variables.tf
@@ -14,6 +14,11 @@
  * limitations under the License.
  */
 
+variable "name" {
+  description = "Name prefix used for naming child radlab resources"
+  type        = string
+}
+
 variable "billing_account_id" {
   description = "Billing Account associated to the GCP Resources.  {{UIMeta group=0 order=3 updatesafe }}"
   type        = string
@@ -144,7 +149,7 @@ variable "machine_type" {
 variable "network_name" {
   description = "Name of the network to be created. {{UIMeta group=2 order=2 }}"
   type        = string
-  default     = "ai-notebook"
+  default     = null
 }
 
 variable "notebook_count" {
@@ -204,7 +209,7 @@ variable "set_trustedimage_project_policy" {
 variable "subnet_name" {
   description = "Name of the subnet where to deploy the Notebooks. {{UIMeta group=2 order=4 }}"
   type        = string
-  default     = "subnet-ai-notebook"
+  default     = null  
 }
 
 variable "trusted_groups" {
